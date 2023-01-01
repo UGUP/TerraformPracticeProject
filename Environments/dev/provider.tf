eda9d7236d3496996481d87349d7080df36829b0
UGUP <upma121gupta@gmail.com>
2023-01-01 20:05:30 +0530

    provider added

--- Environments/dev/provider.tf
+++ Environments/dev/provider.tf
@@ -8,9 +8,17 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+backend "s3" {
+    bucket = "s3-c38"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
+
