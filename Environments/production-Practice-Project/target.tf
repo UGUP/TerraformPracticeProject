897c1b3202514c17821e607ccefdf05f555f5224
UGUP <upma121gupta@gmail.com>
2023-01-09 18:37:37 +0530

    VPC for practice project ec2 and sg

--- Environments/production-Practice-Project/target.tf
+++ Environments/production-Practice-Project/target.tf
@@ -1,31 +1,28 @@
-module "target_group_practice_project" {
-  source = "terraform-aws-modules/alb/aws"
-  version = "8.2.1"
-  name = "target-practice-project"
+# module "target_group_practice_project" {
+#   source = "terraform-aws-modules/alb/aws"
+#   version = "8.2.1"
+#   name = "target-practice-project"
 
-  # Remove these arguments:
-  # port = 80
-  # protocol = "HTTP"
+#   # Remove these arguments:
+#   # port = 80
+#   # protocol = "HTTP"
 
-  vpc_id = module.VPC_PRACTICE_PROJECT.vpc_id
+#   vpc_id = module.VPC_PRACTICE_PROJECT.vpc_id
 
-  # Use the target_groups argument instead of targets:
-  target_groups = [
-    {
-      id = module.EC2_Practice-Project.id
-      port = 80
-    }  
-  ]
-}
+#   # Use the target_groups argument instead of targets:
+#   target_groups = [
+#     {
+#       id = module.EC2_Practice-Project.id
+#       port = 80
+#     }  
+#   ]
+# }
 
 
-resource "aws_lb_target_group" "target_group_practice_project" {
-  name     = "tf-example-lb-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
+# # resource "aws_lb_target_group" "target_group_practice_project" {
+# #   name     = "target_group_practice_project"
+# #   port     = 80
+# #   protocol = "HTTP"
+# #   vpc_id   = module.VPC_PRACTICE_PROJECT.vpc_id
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+# # }
