633da2d710cb8210558396a24d7c200df11e5891
UGUP <upma121gupta@gmail.com>
2023-01-09 20:17:34 +0530

    VPC for practice project ec2 and sg

--- Environments/production-Practice-Project/EC2.tf
+++ Environments/production-Practice-Project/EC2.tf
@@ -5,11 +5,11 @@
 module "EC2_Practice-Project" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   name = "EC2"
-  ami                    = "ami-0a6b2839d44d781b2"
+  ami                    = data.aws_ami.NGINX
   instance_type          = var.instanceType
   key_name               = var.key
   monitoring             = true
-  vpc_security_group_ids = [module.VPC_PRACTICE_PROJECT.vpc_id]
+  vpc_security_group_ids = [module.SG_PRACTICE_PROJECT.security_group_id]
   subnet_id              = module.VPC_PRACTICE_PROJECT.private_subnets[0]
 
   tags = {
