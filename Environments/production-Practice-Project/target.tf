3d86ce67fd12740f462236b8ed4e32fe336dd360
UGUP <upma121gupta@gmail.com>
2023-01-09 18:06:32 +0530

    VPC for practice project ec2 and sg

--- Environments/production-Practice-Project/target.tf
+++ Environments/production-Practice-Project/target.tf
@@ -2,10 +2,15 @@ module "target_group" {
   source = "terraform-aws-modules/alb/aws"
   version = "8.2.1"
   name = "target-practice-project"
-  port = 80
-  protocol = "HTTP"
+
+  # Remove these arguments:
+  # port = 80
+  # protocol = "HTTP"
+
   vpc_id = module.VPC_PRACTICE_PROJECT.vpc_id
-  targets = [
+
+  # Use the target_groups argument instead of targets:
+  target_groups = [
     {
       id = module.EC2_Practice-Project.id
       port = 80
